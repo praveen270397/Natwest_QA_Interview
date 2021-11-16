cab02c3bc93d0c9e8a16c6bb850320717c522abc
Praveen <praveen270397@gmail.com>
2021-11-16 18:47:50 +0530

    master commit

--- src/main/java/com/pages/AddToCartPage.java
+++ src/main/java/com/pages/AddToCartPage.java
@@ -1,5 +1,4 @@
 package com.pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -13,16 +12,12 @@ public class AddToCartPage {
 	}
 
 	private By Add_to_cart =  By.xpath("//button[@name = 'Submit']/span[contains(text(),'Add to cart')]");
-
 	public void addToCart() {
+
 		 driver.findElement(Add_to_cart).click();
 	}
 
-	
-	
-	
-	
-
+xbxfbgsdf
 
 
 }
